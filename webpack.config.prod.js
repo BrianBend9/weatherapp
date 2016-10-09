4a033139c8fa9837015d6be091ae1951cd62cb72
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-10-09 11:08:52 -0400

    Modify bundling script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
-const webpack = require('webpack');
+const path = require('path');
 const defaultConfig = require('./webpack.common');
+const webpack = require('webpack');
 
 defaultConfig.plugins.push(
   new webpack.optimize.UglifyJsPlugin({ minimize: true })
