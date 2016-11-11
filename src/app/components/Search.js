9af18c3e6b3b709c8d12985c74cf2229b5656586
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-11-11 14:59:18 -0500

    Fix error in handleSubmit function

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -25,7 +25,7 @@ class Search extends Component {
   handleSubmit(event) {
     event.preventDefault();
     this.props.routerActions.push(`/overview/${this.state.location}`);
-    this.props.actionCreators.getLocationAndForecast(location);
+    this.props.actionCreators.getLocationAndForecast(this.state.location);
     this.setState({
       location: '',
     });
